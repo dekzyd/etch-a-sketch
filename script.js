74af4f0b33c3aefdbe06362def5dc67091c44d88
Adekoya <oladayo@spotter.ai>
2026-02-11 12:34:45 +0100

    divs change color when your mouse passes over them

--- script.js
+++ script.js
@@ -5,5 +5,8 @@ grid.classList.add('container')
 for(let i = 0; i < 256; i++) {
     const square = document.createElement('div')
     square.classList.add('square')
+    square.addEventListener('mouseover', () => {
+        square.style.backgroundColor = 'black'
+    })
     grid.appendChild(square)
 }
